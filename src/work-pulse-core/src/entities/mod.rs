98338f4d0a425d41a502feeb3fcbb1d76265696b
Walter <wrstocke@googlemail.com>
2025-05-27 12:43:20 +0200

    refactor: rename Booking to Activity

--- src/work-pulse-core/src/entities/mod.rs
+++ src/work-pulse-core/src/entities/mod.rs
@@ -3,51 +3,68 @@ use std::fmt::Display;
 use thiserror::Error;
 use uuid::Uuid;
 
-/// Errors that can occur when working with `BookingId`.
+/// Errors that can occur when working with `ActivityId`.
 #[derive(Error, Clone, Debug, Eq, PartialEq)]
-pub enum BookingIdError {
-    /// The id is not a valid booking id.
-    #[error("The provided id is not a valid booking id: {0}")]
+pub enum ActivityIdError {
+    /// The given string is not a valid activity id.
+    #[error("The provided string is not a valid activity id: {0}")]
     NotAValidId(String),
 }
 
-/// The unique identifier for a booking.
+/// The unique identifier for an activity.
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
-pub struct BookingId(pub Uuid);
+pub struct ActivityId(pub Uuid);
 
-impl BookingId {
+impl ActivityId {
+    /// Creates a new `ActivityId` with a random UUID.
     pub fn new() -> Self {
         Self(Uuid::new_v4())
     }
 
-    pub fn parse_str(s: &str) -> Result<Self, BookingIdError> {
+    /// Parses a string into an `ActivityId`.
+    /// 
+    /// Returns an error if an invalid UUID is provided.
+    /// 
+    /// # Arguments
+    /// 
+    /// - `s`: A string slice that represents a UUID.
+    pub fn parse_str(s: &str) -> Result<Self, ActivityIdError> {
         Uuid::parse_str(s)
             .map(Self)
-            .map_err(|_| BookingIdError::NotAValidId(s.to_string()))
+            .map_err(|_| ActivityIdError::NotAValidId(s.to_string()))
     }
 }
 
-impl Display for BookingId {
+impl Display for ActivityId {
+    /// Formats the `ActivityId` as a string.
+    /// 
+    /// # Arguments
+    /// 
+    /// - `f`: A mutable reference to a formatter.
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}", self.0)
     }
 }
 
-/// Represents a booking in the system.
+/// Represents an activity that the user did during his working day.
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
-pub struct Booking {
-    /// The unique identifier for the booking.
-    id: BookingId,
+pub struct Activity {
+    /// The unique identifier for the activity.
+    id: ActivityId,
 }
 
-impl Booking {
-    /// Creates a new booking.
-    pub fn new(id: BookingId) -> Self {
+impl Activity {
+    /// Creates a new activity.
+    /// 
+    /// # Arguments
+    /// 
+    /// - `id`: The unique identifier for the activity.
+    pub fn new(id: ActivityId) -> Self {
         Self { id }
     }
 
-    /// Returns the unique identifier for the booking.
-    pub fn id(&self) -> &BookingId {
+    /// Returns the unique identifier for the activity.
+    pub fn id(&self) -> &ActivityId {
         &self.id
     }
 }
