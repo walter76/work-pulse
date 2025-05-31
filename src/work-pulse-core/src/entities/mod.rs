16998c0998a141e0386ac7e1569b19a918851c6e
Walter <wrstocke@googlemail.com>
2025-05-31 14:57:00 +0200

    refactor: minor cleanup

--- src/work-pulse-core/src/entities/mod.rs
+++ src/work-pulse-core/src/entities/mod.rs
@@ -1,18 +1,2 @@
 pub mod activity;
 pub mod pam;
-
-// There are two use cases for the activity:
-//   1. The user starts the activity and then stops it later.
-//   2. The user records an activity that was done in the past.
-// How to model the two use cases in the domain with entities?
-
-/// Represents a tracker for activities.
-/// 
-/// It is used to track an activity that the user is currently doing. Supports
-/// starting, stopping, suspending, and resuming activities.
-/// 
-/// The ActivityTracker is recording the activity after it is finished in the
-/// ActivitiesList.
-/// 
-/// TODO The `ActivityTracker` will be implemented later.
-pub struct ActivityTracker;
--- src/work-pulse-core/src/use_cases/mod.rs
+++ src/work-pulse-core/src/use_cases/mod.rs
@@ -1,2 +1,18 @@
 pub mod activities_list;
 pub mod pam_categories_list;
+
+// There are two use cases for the activity:
+//   1. The user starts the activity and then stops it later.
+//   2. The user records an activity that was done in the past.
+// How to model the two use cases in the domain with entities?
+
+/// Represents a tracker for activities.
+/// 
+/// It is used to track an activity that the user is currently doing. Supports
+/// starting, stopping, suspending, and resuming activities.
+/// 
+/// The ActivityTracker is recording the activity after it is finished in the
+/// ActivitiesList.
+/// 
+/// TODO The `ActivityTracker` will be implemented later.
+pub struct ActivityTracker;
