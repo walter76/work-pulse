c394b41f0101b8a199c078001b6b9e88878dea4a
Walter <wrstocke@googlemail.com>
2025-10-05 16:18:04 +0200

    fix: correct typo in build.cmd

--- build.cmd
+++ build.cmd
@@ -2,9 +2,9 @@
 echo Building container images for work-pulse ...
 
 echo Building backend container image...
-docker build -f src/work-pulse-service.Dockerfile -t workpulse-service ./src
+docker build -f src/work-pulse-service.Dockerfile -t work-pulse-service ./src
 
 echo Building frontend container image...
-docker build -f src/work-pulse-app.Dockerfile -t workpulse-app ./src
+docker build -f src/work-pulse-app.Dockerfile -t work-pulse-app ./src
 
 echo Build complete!
