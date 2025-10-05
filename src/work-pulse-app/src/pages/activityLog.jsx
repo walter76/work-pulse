ffd027615cdec49209e3475168c2deae1edbdd63
Walter <wrstocke@googlemail.com>
2025-10-05 17:52:32 +0200

    refactor: carve-out dateUtils

--- src/work-pulse-app/src/pages/activityLog.jsx
+++ src/work-pulse-app/src/pages/activityLog.jsx
@@ -5,67 +5,7 @@ import { Delete, Edit, Refresh } from '@mui/icons-material'
 import axios from 'axios'
 
 import { API_BASE_URL } from '../config/api'
-
-// Helper function to get week number from a date
-const getWeekNumber = (date) => {
-  const d = new Date(date)
-  d.setHours(0, 0, 0, 0)
-  d.setDate(d.getDate() + 3 - ((d.getDay() + 6) % 7))
-  const week1 = new Date(d.getFullYear(), 0, 4)
-  return 1 + Math.round(((d - week1) / 86400000 - 3 + ((week1.getDay() + 6) % 7)) / 7)
-}
-
-// Helper function to get the start and end dates of the week for a given date (Monday to Sunday)
-const getWeekRange = (date) => {
-  const d = new Date(date)
-  const day = d.getDay()
-  const diffToMonday = d.getDate() - day + (day === 0 ? -6 : 1)
-  const monday = new Date(d.setDate(diffToMonday))
-  const sunday = new Date(monday)
-  sunday.setDate(monday.getDate() + 6)
-
-  return {
-    start: monday.toISOString().split('T')[0],
-    end: sunday.toISOString().split('T')[0],
-  }
-}
-
-// Function to group activities by week
-const groupActivitiesByWeek = (activities) => {
-  const grouped = {}
-
-  activities.forEach((activity) => {
-    const weekNumber = getWeekNumber(activity.date)
-    const year = new Date(activity.date).getFullYear()
-    const weekKey = `${year}-W${weekNumber.toString().padStart(2, '0')}`
-    const weekRange = getWeekRange(activity.date)
-
-    if (!grouped[weekKey]) {
-      grouped[weekKey] = {
-        weekNumber,
-        year,
-        weekRange,
-        activities: [],
-      }
-    }
-
-    grouped[weekKey].activities.push(activity)
-  })
-
-  return grouped
-}
-
-// Function to get the first and last day of the current month
-const getCurrentMonthRange = () => {
-  const now = new Date()
-  const firstDay = new Date(now.getFullYear(), now.getMonth(), 1)
-  const lastDay = new Date(now.getFullYear(), now.getMonth() + 1, 0)
-
-  return {
-    start: firstDay.toISOString().split('T')[0],
-    end: lastDay.toISOString().split('T')[0],
-  }
-}
+import { getCurrentMonthRange, groupActivitiesByWeek } from '../lib/dateUtils'
 
 const ActivityLog = () => {
   const currentMonthRange = getCurrentMonthRange()
