e9e176d81b2c5e72522a4d344980a0cabbf0dafb
Walter <wrstocke@googlemail.com>
2025-10-14 09:42:23 +0200

    refactor: cleanup code for daily_report use cases

--- src/work-pulse-core/src/use_cases/daily_report.rs
+++ src/work-pulse-core/src/use_cases/daily_report.rs
@@ -5,19 +5,27 @@ use crate::{adapters::ActivitiesListRepository, entities::activity::Activity};
 /// Represents a daily report containing activities and total duration for a specific date.
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct DailyReport {
+    /// The date of the report. Also represents the date of the activities used to generate the report.
     date: NaiveDate,
+
+    /// The list of activities recorded for the report date.
     activities: Vec<Activity>,
+
+    /// The total duration of all activities recorded for the report date.
     total_duration: Duration,
 }
 
 impl DailyReport {
+    /// Creates a new `DailyReport` for the specified date using the provided repository.
+    ///
+    /// # Arguments
+    ///
+    /// - `date`: The date for which the report is generated.
+    /// - `repository`: The repository used to fetch activities for the specified date.
     pub fn new(date: NaiveDate, repository: &dyn ActivitiesListRepository) -> Self {
         let activities = repository.get_by_date(date);
 
-        let total_duration = activities
-            .iter()
-            .map(|activity| activity.duration())
-            .sum();
+        let total_duration = activities.iter().map(|activity| activity.duration()).sum();
 
         DailyReport {
             date,
@@ -26,14 +34,17 @@ impl DailyReport {
         }
     }
 
+    /// Returns the date of the report.
     pub fn date(&self) -> NaiveDate {
         self.date
     }
 
+    /// Returns the list of activities recorded for the report date.
     pub fn activities(&self) -> &[Activity] {
         &self.activities
     }
 
+    /// Returns the total duration of all activities recorded for the report date.
     pub fn total_duration(&self) -> Duration {
         self.total_duration
     }
@@ -58,20 +69,20 @@ mod tests {
         let repository = Arc::new(Mutex::new(InMemoryActivitiesListRepository::new()));
         let mut activities_list = ActivitiesList::new(repository.clone());
 
-        let date = NaiveDate::from_ymd_opt(2023, 10, 1).expect("Valid date");
+        let date = NaiveDate::from_ymd_opt(2023, 10, 1).expect("Valid activity date");
 
         let activity1 = activities_list.record(
             date,
-            NaiveTime::from_hms_opt(9, 0, 0).unwrap(),
-            Some(NaiveTime::from_hms_opt(10, 0, 0).unwrap()),
+            NaiveTime::from_hms_opt(9, 0, 0).expect("Valid activity start time"),
+            Some(NaiveTime::from_hms_opt(10, 0, 0).expect("Valid activity end time")),
             AccountingCategoryId::new(),
             "Task 1".to_string(),
         );
 
         let activity2 = activities_list.record(
             date,
-            NaiveTime::from_hms_opt(11, 0, 0).unwrap(),
-            Some(NaiveTime::from_hms_opt(12, 30, 0).unwrap()),
+            NaiveTime::from_hms_opt(11, 0, 0).expect("Valid activity start time"),
+            Some(NaiveTime::from_hms_opt(12, 30, 0).expect("Valid activity end time")),
             AccountingCategoryId::new(),
             "Task 2".to_string(),
         );
@@ -79,7 +90,10 @@ mod tests {
         let daily_report = DailyReport::new(date, &*repository.lock().unwrap());
 
         assert_eq!(daily_report.date(), date);
-        assert_eq!(daily_report.activities(), &[activity1.clone(), activity2.clone()]);
+        assert_eq!(
+            daily_report.activities(),
+            &[activity1.clone(), activity2.clone()]
+        );
         assert_eq!(
             daily_report.total_duration(),
             Duration::minutes(90) + Duration::minutes(60)
