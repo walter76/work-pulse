52e76f4f9ee177a9ce24e33db7f58d3b06c78238
Walter <wrstocke@googlemail.com>
2025-10-14 09:25:07 +0200

    refactor: cleanup code for repository factory for in-memory repositories

--- src/work-pulse-core/src/infra/repositories/in_memory/mod.rs
+++ src/work-pulse-core/src/infra/repositories/in_memory/mod.rs
@@ -1,19 +1,22 @@
-pub mod activities_list;
 pub mod accounting_categories_list;
+pub mod activities_list;
 
 use std::sync::{Arc, Mutex};
 
 use crate::{
     adapters::{AccountingCategoriesListRepository, ActivitiesListRepository},
     infra::repositories::in_memory::{
-        activities_list::InMemoryActivitiesListRepository,
         accounting_categories_list::InMemoryAccountingCategoriesListRepository,
+        activities_list::InMemoryActivitiesListRepository,
     },
 };
 
 /// Represents a factory for creating in-memory repositories.
 pub struct RepositoryFactory {
+    /// Repository for managing activities.
     pub activities_list_repository: Arc<Mutex<dyn ActivitiesListRepository>>,
+
+    /// Repository for managing accounting categories.
     pub accounting_categories_list_repository: Arc<Mutex<dyn AccountingCategoriesListRepository>>,
 }
 
