b2c0531387fbb2c2f70a867472ecdd9e0af840cf
Walter <wrstocke@googlemail.com>
2025-10-24 09:40:18 +0200

    feat: add command line parameter to start service with in-memory repositories for dev purposes

--- src/work-pulse-service/src/main.rs
+++ src/work-pulse-service/src/main.rs
@@ -1,5 +1,6 @@
 mod services;
 
+use clap::Parser;
 use std::io::Error;
 use std::net::{Ipv4Addr, SocketAddr};
 use std::sync::Arc;
@@ -11,13 +12,18 @@ use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 use utoipa::OpenApi;
 use utoipa_axum::router::OpenApiRouter;
 use utoipa_swagger_ui::SwaggerUi;
+use work_pulse_core::adapters::{AccountingCategoriesListRepository, ActivitiesListRepository};
 
-use services::accounting_categories_service;
-use work_pulse_core::infra::repositories::postgres::PsqlConnection;
-use work_pulse_core::infra::repositories::postgres::accounting_categories_list::PsqlAccountingCategoriesListRepository;
-use work_pulse_core::infra::repositories::postgres::activities_list::PsqlActivitiesListRepository;
-
-use crate::services::activities_list_service;
+use work_pulse_core::infra::repositories::{
+    in_memory::{
+        accounting_categories_list::InMemoryAccountingCategoriesListRepository,
+        activities_list::InMemoryActivitiesListRepository,
+    },
+    postgres::{
+        PsqlConnection, accounting_categories_list::PsqlAccountingCategoriesListRepository,
+        activities_list::PsqlActivitiesListRepository,
+    },
+};
 
 mod prelude {
     pub const ACTIVITIES_LIST_SERVICE_TAG: &str = "activities-list-service";
@@ -29,6 +35,19 @@ mod prelude {
 const CONNECTION_STRING: &str = "postgres://workpulse:supersecret@localhost:5432/workpulse";
 
 // TODO Implement health check service
+
+#[derive(clap::Parser)]
+#[command(
+    name = "Work Pulse API",
+    version = "0.1",
+    author = "Walter Stocker <wrstocke@googlemail.com>",
+    about = "Work Pulse API Server"
+)]
+struct Cli {
+    /// Use in-memory repositories instead of PostgreSQL
+    #[arg(long, default_value_t = false)]
+    use_in_memory_repositories: bool,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
@@ -50,30 +69,20 @@ async fn main() -> Result<(), Error> {
         .with(tracing_subscriber::fmt::layer())
         .init();
 
-    let (accounting_categories_repository, activities_list_repository) =
-        create_psql_repositories().await;
+    let cli = Cli::parse();
+    let (router, api) = if cli.use_in_memory_repositories {
+        let (accounting_categories_repository, activities_list_repository) =
+            create_in_memory_repositories().await;
 
-    let (router, api) = OpenApiRouter::with_openapi(ApiDoc::openapi())
-        .nest(
-            "/api/v1/accounting-categories",
-            accounting_categories_service::router(accounting_categories_repository.clone()),
-        )
-        .nest(
-            "/api/v1/activities",
-            activities_list_service::router(
-                activities_list_repository.clone(),
-                accounting_categories_repository.clone(),
-            ),
-        )
-        .nest(
-            "/api/v1/daily-report",
-            services::daily_report_service::router(activities_list_repository.clone()),
-        )
-        .nest(
-            "/api/v1/weekly-report",
-            services::weekly_report_service::router(activities_list_repository.clone()),
-        )
-        .split_for_parts();
+        create_open_api_router(accounting_categories_repository, activities_list_repository)
+            .split_for_parts()
+    } else {
+        let (accounting_categories_repository, activities_list_repository) =
+            create_psql_repositories().await;
+
+        create_open_api_router(accounting_categories_repository, activities_list_repository)
+            .split_for_parts()
+    };
 
     let router =
         router.merge(SwaggerUi::new("/swagger-ui").url("/api-docs/openapi.json", api.clone()));
@@ -97,6 +106,11 @@ async fn main() -> Result<(), Error> {
     axum::serve(listener, router.into_make_service()).await
 }
 
+/// Creates PostgreSQL repositories for production use.
+///
+/// Returns a tuple containing:
+/// - An `Arc<Mutex<PsqlAccountingCategoriesListRepository>>`
+/// - An `Arc<Mutex<PsqlActivitiesListRepository>>`
 async fn create_psql_repositories() -> (
     Arc<Mutex<PsqlAccountingCategoriesListRepository>>,
     Arc<Mutex<PsqlActivitiesListRepository>>,
@@ -113,4 +127,66 @@ async fn create_psql_repositories() -> (
         psql_accounting_categories_repository,
         psql_activities_list_repository,
     )
+}
+
+/// Creates in-memory repositories for testing purposes.
+///
+/// Returns a tuple containing:
+/// - An `Arc<Mutex<InMemoryAccountingCategoriesListRepository>>`
+/// - An `Arc<Mutex<InMemoryActivitiesListRepository>>`
+async fn create_in_memory_repositories() -> (
+    Arc<Mutex<InMemoryAccountingCategoriesListRepository>>,
+    Arc<Mutex<InMemoryActivitiesListRepository>>,
+) {
+    let in_memory_accounting_categories_repository =
+        Arc::new(Mutex::new(InMemoryAccountingCategoriesListRepository::new()));
+    let in_memory_activities_list_repository =
+        Arc::new(Mutex::new(InMemoryActivitiesListRepository::new()));
+
+    (
+        in_memory_accounting_categories_repository,
+        in_memory_activities_list_repository,
+    )
+}
+
+/// Creates an OpenAPI router with the provided repositories.
+///
+/// # Arguments
+///
+/// - `accounting_categories_repository`: An `Arc<Mutex<R>>` where `R` implements `AccountingCategoriesListRepository`.
+/// - `activities_list_repository`: An `Arc<Mutex<T>>` where `T` implements `ActivitiesListRepository`.
+///
+/// # Returns
+///
+/// An `OpenApiRouter` configured with the provided repositories.
+fn create_open_api_router<R, T>(
+    accounting_categories_repository: Arc<Mutex<R>>,
+    activities_list_repository: Arc<Mutex<T>>,
+) -> OpenApiRouter
+where
+    R: AccountingCategoriesListRepository + Send + Sync + 'static,
+    T: ActivitiesListRepository + Send + Sync + 'static,
+{
+    OpenApiRouter::new()
+        .nest(
+            "/api/v1/accounting-categories",
+            services::accounting_categories_service::router(
+                accounting_categories_repository.clone(),
+            ),
+        )
+        .nest(
+            "/api/v1/activities",
+            services::activities_list_service::router(
+                activities_list_repository.clone(),
+                accounting_categories_repository.clone(),
+            ),
+        )
+        .nest(
+            "/api/v1/daily-report",
+            services::daily_report_service::router(activities_list_repository.clone()),
+        )
+        .nest(
+            "/api/v1/weekly-report",
+            services::weekly_report_service::router(activities_list_repository.clone()),
+        )
 }
